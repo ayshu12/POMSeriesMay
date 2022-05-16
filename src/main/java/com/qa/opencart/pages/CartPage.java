181e6fefe812553d5cdbd8fb9785fada2421e16e
Aryan Ayswarya <ayshu12@gmail.com>
2022-05-16 22:04:44 +0530

    updated cart page as per review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,12 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 
+	private By cartButton= By.id("cart");
 	public CartPage()
 	{
 		System.out.println("cart page....");
 	}
 	public void addToCart() {
 		System.out.println("Add to cart");
+		System.out.println("addd feature is done");
 	}
 }
