1444f29feb797bd67992c76ee63fddbf863dffae
Aryan Ayswarya <ayshu12@gmail.com>
2022-05-17 12:37:07 +0530

    updated product ayshu

--- src/main/java/com/qa/opencart/pages/ProductDemo.java
+++ src/main/java/com/qa/opencart/pages/ProductDemo.java
@@ -1,5 +1,10 @@
 package com.qa.opencart.pages;
 
 public class ProductDemo {
-
+ 
+	public ProductDemo() {
+		System.out.println("Product demo");
+		System.out.println("Product demo 3");
+		System.out.println("Product demo 6666");
+	}
 }
